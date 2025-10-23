f643489e22227941fa1442c1c32c66cd060afffd
Yunic04 <juliusnicholas29@gmail.com>
2025-10-23 08:58:01 +0700

    simple blackjack game

--- index.js
+++ index.js
@@ -1,15 +1,64 @@
-let homeScoreEl = document.getElementById("home-score")
-let guestScoreEl = document.getElementById("guest-score")
-let homeScore = 0
-let guestScore = 0
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+// let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.getElementById("cards-el")
 
+let player = {
+    name: "Player",
+    chips: 145
+}
 
-function incrementScore(score, side) {
-    if (side == "home"){
-        homeScore += score
-        homeScoreEl.textContent = homeScore 
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
+function startGame(){
+    cards = [getRandomCard(), getRandomCard()]
+    sum = cards[0] + cards[1]
+    isAlive = true
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (i=0; i<cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
     } else {
-        guestScore += score
-        guestScoreEl.textContent = guestScore
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+function newCard(){
+    if (isAlive && !hasBlackJack) {
+        let card = getRandomCard()
+        cards.push(card)
+        sum += card
+        renderGame()
+    }
+}
+
+function getRandomCard() {
+    // return Math.floor(Math.random() * 10) + 2
+    let randomCard = Math.floor( Math.random() * 13) + 1
+    if (randomCard === 1) {
+        return 11
+    } else if (randomCard > 10) {
+        return 10
+    } else {
+        return randomCard
     }
 }
--- oldStuff/oldJS.js
+++ oldStuff/oldJS.js
@@ -140,3 +140,20 @@
 //     let sum = num1 / num2
 //     sumEl.textContent = "Sum: " + sum
 // }
+
+
+// let homeScoreEl = document.getElementById("home-score")
+// let guestScoreEl = document.getElementById("guest-score")
+// let homeScore = 0
+// let guestScore = 0
+
+
+// function incrementScore(score, side) {
+//     if (side == "home"){
+//         homeScore += score
+//         homeScoreEl.textContent = homeScore 
+//     } else {
+//         guestScore += score
+//         guestScoreEl.textContent = guestScore
+//     }
+// }
