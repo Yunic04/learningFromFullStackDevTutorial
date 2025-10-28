f3f7dce4deaa37e83683adc58f79533757b16586
Yunic04 <juliusnicholas29@gmail.com>
2025-10-28 16:39:55 +0700

    unit converter

--- index.js
+++ index.js
@@ -1,43 +1,20 @@
 const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const deleteBtn = document.getElementById("delete-btn")
-const ulEl = document.getElementById("ul-el")
-const tabBtn = document.getElementById("tab-btn")
+const convertBtn = document.getElementById("convert-btn")
+const lengthEl = document.getElementById("length-el")
+const volumeEl = document.getElementById("volume-el")
+const massEl = document.getElementById("mass-el")
 
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-let myLeads = leadsFromLocalStorage ? leadsFromLocalStorage : []
+let value = 0
 
-renderLeads(myLeads)
+convertBtn.addEventListener("click", function(){
+    value = Number(inputEl.value)
+    
+    lengthEl.textContent = `${value} meters = ${(value*3.281).toFixed(3)} feet |
+     ${value} feet = ${(value/3.281).toFixed(3)} meters`
 
-inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEl.value)
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLead(inputEl.value)
-    inputEl.value = ""
+    volumeEl.textContent = `${value} liters = ${(value/3.875).toFixed(3)} gallons |
+     ${value} gallons = ${(value*3.875).toFixed(3)} liters`
+
+    massEl.textContent = `${value} kilos = ${(value*2.205).toFixed(3)} pounds |
+     ${value} pounds = ${(value/2.205).toFixed(3)} kilos`
 })
-
-tabBtn.addEventListener("click", function(){
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads))
-        renderLead(tabs[0].url)
-    })
-})
-
-deleteBtn.addEventListener("dblclick", function(){
-    localStorage.clear()
-    myLeads = []
-    ulEl.innerHTML = ''
-})
-
-function renderLead(lead){
-    ulEl.innerHTML += `<li> <a href="${lead}" target="_blank"> ${lead}</a> </li>`
-}
-
-function renderLeads(leads) {
-    let listItems = ""
-    for (let i = 0; i<leads.length; i++){
-        listItems += `<li> <a href="${leads[i]}" target="_blank"> ${leads[i]}</a> </li>`
-    }
-    ulEl.innerHTML = listItems
-}
