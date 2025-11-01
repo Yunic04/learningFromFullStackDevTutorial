fcadc5f77011f204703889039447afa0c620af78
Yunic04 <juliusnicholas29@gmail.com>
2025-11-01 11:07:31 +0700

    simplified code

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
 import { propertyForSaleArr } from "./properties/propertyForSaleArr.js"
 import { placeholderPropertyObj } from "./properties/placeholderPropertyObj.js"
 
-function getPropertyHtml(propertyObj = placeholderPropertyObj){
-    if (propertyObj.length) {
-        return propertyObj.map(property => getHtml(property)).join('')
-    } else {
-        return getHtml(propertyObj)
-    }
+const placeholderObjArr = [placeholderPropertyObj]
+
+function getPropertyHtml(properties = placeholderObjArr){
+    return properties.map(property => {
+        const { propertyLocation, priceGBP, roomsM2, comment, image } = property
+        const totalSize = roomsM2.reduce((total, size) => total + size, 0)
+        return `
+        <section class="card">
+            <img src="images/${image}">
+            <div class="card-right">
+                <h2>${propertyLocation}</h2>
+                <h3>£${priceGBP}</h3>
+                <p>${comment}</p>
+                <h3>${totalSize} m²</h3>
+            </div>
+        </section>
+        `
+    }).join('')
 }
 
-function getHtml(property){
-    const { propertyLocation, priceGBP, roomsM2, comment, image} = property
-    const totalSize = roomsM2.reduce((total, size) => total + size, 0)
-    return `
-    <section class="card">
-        <img src="images/${image}">
-        <div class="card-right">
-            <h2>${propertyLocation}</h2>
-            <h3>£${priceGBP}</h3>
-            <p>${comment}</p>
-            <h3>${totalSize} m²</h3>
-        </div>
-    </section>
-    `
-}
-
-// document.getElementById('container').innerHTML = getPropertyHtml()
-document.getElementById('container').innerHTML = getPropertyHtml(propertyForSaleArr)
+document.getElementById('container').innerHTML = getPropertyHtml()
+// document.getElementById('container').innerHTML = getPropertyHtml(propertyForSaleArr)
