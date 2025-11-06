9e6a6f745221d679c80f2aca56b807e0278e7416
Yunic04 <juliusnicholas29@gmail.com>
2025-11-06 12:02:37 +0700

    some progress

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { dates } from '/utils/dates.js'
+import { POLYGON_API_KEY } from './apiKeys.js'
 
 const tickers = document.getElementById('tickers')
 const generateBtn = document.getElementById('generate-btn')
@@ -32,25 +33,20 @@ async function fetchStockData() {
     document.getElementById('main').style.display = "none"
     loadingArea.style.display = "block"
     try {
-        // const stockData = await Promise.all(tickerArr.map(async (ticker) => {
-        //     const url = `https://api.polygon.io/v2/aggs/ticker/${ticker}/range/1/day/${dates.
-        //     startDate}/${dates.endDate}?apiKey=${process.env.POLYGON_API_KEY}`
-        //     const response = await fetch(url)
-        //     const data = await response.json()
-        //     const status = await response.status
-        //     if (status === 200) {
-        //         apiMessage.textContent = 'Creating report...'
-        //         return data
-        //     } else {
-        //         loadingArea.innerHTML = 'There was an error fetching stock data'
-        //     }
-        // }))
-        // fetchReport(stockData.join(''))
-        setTimeout(()=>{
-            document.getElementById('main').style.display = "block"
-            loadingArea.style.display = "none"
-            tickers.textContent = 'no api key so have this random message instead :)'
-        }, 2000)
+        const stockData = await Promise.all(tickerArr.map(async (ticker) => {
+            const url = `https://api.polygon.io/v2/aggs/ticker/${ticker}/range/1/day/${dates.startDate}/${dates.endDate}?apiKey=${POLYGON_API_KEY}`
+            const response = await fetch(url)
+            const data = await response.json()
+            const status = await response.status
+            if (status === 200) {
+                apiMessage.textContent = 'Creating report...'
+                return data
+            } else {
+                loadingArea.innerHTML = 'There was an error fetching stock data'
+            }
+        }))
+        console.log(stockData)
+        fetchReport(stockData.join(''))
     } catch (err) {
         loadingArea.innerHTML = 'There was an error fetching stock data'
         console.log(`error: ${err}`)
@@ -58,7 +54,35 @@ async function fetchStockData() {
 }
 
 async function fetchReport(data) {
-    console.log('hi')
+    const messages = [
+        {
+            role: 'system',
+            content: `You are a trading guru. Given data on share prices over the past 3 days, 
+            write a report of no more than 150 words describing the stocks performance and 
+            recommending whether to buy, hold or sell.`
+        },
+        {
+            role: 'user',
+            content: data
+        }
+    ]
+
+    try {
+        const openai = new OpenAI({
+            dangerouslyAllowBrowser: true
+        })
+        const response = await openai.chat.completions.create({
+            model
+            : 'gpt-4',
+            messages: messages
+        })
+        renderReport(response.choices[0].message.content)
+    } catch(err) {
+        console.log(`Error: ${err}`)
+        document.getElementById('main').style.display = "block"
+        loadingArea.style.display = "none"
+        tickers.textContent = 'no api key so have this random message instead :)'
+    }
 }
 
 function renderReport(output) {
